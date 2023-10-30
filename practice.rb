4c49df5655b4c767e44d98aeeff44271819d12fe
Erika Gonzalez <erikagonzz01@gmail.com>
2023-10-30 17:51:55 -0400

    Add practice problem

--- practice.rb
+++ practice.rb
@@ -2,3 +2,9 @@
 #word with all capital letters.
 word = "howdy"
 puts word.upcase
+
+#Write a program that uses variables to store two numbers, 
+# then prints the numbers added together.
+number1 = 20
+number2 = 15
+puts number1 + number2
