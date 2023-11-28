c5db4b519ea2b150f83a74eea7fbebdb8f125ac0
Erika Gonzalez <erikagonzz01@gmail.com>
2023-11-28 16:58:53 -0500

    Create variables2-05.rb

---
+++ variables2-05.rb
@@ -0,0 +1,7 @@
+# Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
+
+color1 = "red"
+color2 = "orange"
+color3 = "blue"
+
+puts "I like the colors #{color1}, #{color2}, and #{color3}."
