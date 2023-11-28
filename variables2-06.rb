291fc95a53364bddb4acc38f7df4bc1d79744114
Erika Gonzalez <erikagonzz01@gmail.com>
2023-11-28 16:58:55 -0500

    Create variables2-06.rb

---
+++ variables2-06.rb
@@ -0,0 +1,7 @@
+# Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
+puts "What is your name?"
+name = gets.chomp
+
+if name != "Stana"
+  puts "You're not Santa."
+end 
