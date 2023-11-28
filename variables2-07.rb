c00648bbeb636d65be66275a4fd357df05ab78aa
Erika Gonzalez <erikagonzz01@gmail.com>
2023-11-28 16:58:57 -0500

    Create variables2-07.rb

---
+++ variables2-07.rb
@@ -0,0 +1,6 @@
+# Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string concatenation (the + operator).
+
+book_title = "Hunger Games"
+author = "Suzanna Collins"
+
+puts "I like the book " + book_title + " written by " + author +"."
