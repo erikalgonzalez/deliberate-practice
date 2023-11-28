227a55432ce871cea65529ac3904261f12de7303
Erika Gonzalez <erikagonzz01@gmail.com>
2023-11-28 16:59:14 -0500

    Create variables2-08.rb

---
+++ variables2-08.rb
@@ -0,0 +1,6 @@
+# Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string interpolation (the #{} operator).
+
+book_title = "Twilight"
+author = "Stephine Meyer"
+
+puts "Another book I like is #{book_title} by #{author}."
